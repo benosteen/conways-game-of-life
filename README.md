6e8d9de73900cac309853e41de03753c27407b90
Ben O'Steen <bosteen@gmail.com>
2013-02-04 15:01:23 +0000

    Added the main script for music generation and removed the unused ones from the fork

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-Conway's Game of Life
-=====================
+Musical Game of Life
+====================
 
-An ultra-lightweight implementation of Conway's game of life, including a visualizer.
+Inspired by Vihart manipulating music as if it were space and time in her Folding Space time video (http://www.youtube.com/watch?v=WkmPDOq2WfA), I wondered what it would be like if the music 'tape' going through the box could rewrite itself each rotation - cellular automation!
 
-Edit the inputs in `main.py` and then run it. `conway.py` will run the actual
-game, and `visualization.py` will draw the cells on a centered grid.
+So, adapting [patrickfuller's Game Of Life implementation](https://github.com/patrickfuller/conways-game-of-life) (from which this project is forked) and adding in a bit of my MIDI music work from an earlier project, I made this procedural, evolving, abstract musical **thing** that renders the Game of Life state as a sequence of MIDI notes, making for some oddly musical and sometimes disjointed sounds.
 
-The visualization requires the PIL Python library, which can be installed via pip.
+Notes
+=====
+
+*conway.py*
+You will have to adjust the MIDI output (line 11) until you get music notes. If using a USB MIDI device, you'll probably see an LED flashing to show traffic going through it.
+
+You might want to change the note length (line 24), the scale used (line 28) or the initial state (line 132).
--- conway.py
+++ conway.py
@@ -1,23 +1,53 @@
-#!/usr/bin/env python
-"""
-Conway's game of life, final version.
+import pygame
+import pygame.midi
 
-Rules:
- * Any cell with fewer than two neighbors dies (underpopulation)
- * Any cell with more than three neighbors dies (overpopulation)
- * Any empty spot with three neighbors becomes a live cell (reproduction)
+from time import sleep,time
 
-Check out the wikipedia article for more information, including interesting
-starting populations.
-"""
+pygame.midi.init()
 
+# 2 corresponds to my USB MIDI output
+# 0 is often the default software MIDI
+# Trial and Error needed here unfortunately
+m = pygame.midi.Output(2)
+
+SCALES = {'CMAJOR': [0,2,4,5,7,9,11],
+          'BLUES': [0,3,5,6,7,10,11],
+          'MAJORPENT': [0,2,4,7,11],
+          'MINORPENT': [0,3,5,7,10],
+          'JAZZY': [0,2,4,6,8,10],
+          'INSEN': [0,1,5,7,10],
+          'HIRAJOSHI': [0,1,5,7,9],
+          'THIRDS': [0,4,2,5,4,7,5,9,7,11,13,12],
+          'CHROMATIC': [0,1,2,3,4,5,6,7,8,9,10,11]
+          }
+
+note_length = 0.2 # 200ms
+maxbounds = (32,24) # 32 notes long, 24 (3 octaves) high
+
+# Pick a scale from above or manufacture your own
+scale = SCALES['INSEN']
+
+# Notes start at:
+offset = 30
+
+# NB cells run from 0 to 31, and 0 to 23
+
+def boundedpoint(old_point):
+    return (old_point[0] % maxbounds[0], old_point[1] % maxbounds[1])
+
+def neighbors(cell):
+    x, y = cell
+    return [boundedpoint((x + 1, y)), boundedpoint((x - 1, y)), 
+            boundedpoint((x, y + 1)), boundedpoint((x, y - 1)), 
+            boundedpoint((x + 1, y + 1)), boundedpoint((x + 1, y - 1)), 
+            boundedpoint((x - 1, y + 1)), boundedpoint((x - 1, y - 1))]
 
 def run(population, generations=100):
     """Runs Conway's game of life on an initial population."""
     population = set(population)
     for i in range(generations):
         population = evolve(population)
-    return list(population)
+    return population
 
 
 def evolve(population):
@@ -30,13 +60,85 @@ def evolve(population):
     for cell in active_cells:
         count = sum([neighbor in population for neighbor in neighbors(cell)])
         if count == 3 or (count == 2 and cell in population):
-            new_population.add(cell)
+            new_population.add(boundedpoint(cell))
     # Return the new surviving population
     return new_population
 
+def play_midi(notes):
+    for cursor in range(maxbounds[0]):
+        t = time()
+        keys = []
+        if notes.has_key(cursor):
+            for note in notes[cursor]:
+                keys.append(12 * (note / len(scale)) + (scale[note % len(scale)]))
+        for k in keys:
+            m.note_on(k,100)
+        sleep((note_length+t) - time())
+        for k in keys:
+            m.note_off(k,100)
 
-def neighbors(cell):
-    """Returns the neighbors of a given cell."""
-    x, y = cell
-    return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1),
-            (x + 1, y - 1), (x - 1, y + 1), (x - 1, y - 1)]
+def vis(pop):
+    for i in range(maxbounds[1]):
+        line = ""
+        for j in range(maxbounds[0]):
+            if (j,i) in pop:
+                line += "O"
+            else:
+                line += "."
+        print line
+
+def pic_to_set(lifestring):
+    i_pop = set()
+    y = 0
+    for line in lifestring.split("\n"):
+        for idx, char in enumerate(line):
+            if char == "O":
+                i_pop.add((idx,y))
+        y += 1
+    return i_pop
+
+#glider = set([(0,0), (1,0), (2,0), (0,1), (1,2)])
+
+# See http://www.argentum.freeserve.co.uk/lex.htm
+# (Game of Life Lexicon)
+
+beehive_and_dock = """
+...OO.
+..O..O
+...OO.
+......
+.OOOO.
+O....O
+OO..OO"""
+
+glider = """
+OOO
+O..
+.O."""
+
+gospel_gun = """
+........................O...........
+......................O.O...........
+............OO......OO............OO
+...........O...O....OO............OO
+OO........O.....O...OO..............
+OO........O...O.OO....O.O...........
+..........O.....O.......O...........
+...........O...O....................
+............OO......................"""
+
+
+#gameset = pic_to_set(beehive_and_dock)
+gameset = pic_to_set(gospel_gun)
+#gameset = pic_to_set(glider)
+
+for i in range(200):
+    notes = {}
+    gameset = evolve(gameset)
+    vis(gameset)
+    for alivepoint in gameset:
+        x,y = alivepoint
+        if not notes.has_key(x):
+            notes[x] = []
+        notes[x].append(y+offset)
+    play_midi(notes)
