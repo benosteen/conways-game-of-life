35a3d59ee454dfc264ca2ad3fc6af5e281b05fff
Ben O'Steen <bosteen@gmail.com>
2013-02-06 18:37:55 +0000

    Some additional examples added and a way to render the state to an image.

---
+++ methuselah.py
@@ -0,0 +1,20 @@
+from conway import *
+
+#Paul Callahan found, in November 1997, two 10-cell patterns with infinite growth
+# Infinite growth
+# http://www.argentum.freeserve.co.uk/lex_i.htm
+
+offset = 30
+
+state = """
+.............
+..........OO.
+.......O.O...
+.......O.....
+.......O.O...
+.............
+"""
+
+if __name__ == "__main__":
+    play_and_render(state, SCALES['MINORPENT'], cycles = 200, offset = offset)
+    pygame.midi.quit()
